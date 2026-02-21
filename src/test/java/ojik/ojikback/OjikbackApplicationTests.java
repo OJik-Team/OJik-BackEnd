be76ab66053aa9170171227ada83ab695c1d0565
dayoung <dayoungann@naver.com>
2026-02-21 16:37:23 +0900

    Initial commit: Spring Boot project

---
+++ src/test/java/ojik/ojikback/OjikbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ojik.ojikback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjikbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
