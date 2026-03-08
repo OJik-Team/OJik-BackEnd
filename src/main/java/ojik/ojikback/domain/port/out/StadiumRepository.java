fcbe44f81626ba5d43d783f437fa6b5352ae181a
geniusjun <geniusjun4663@gmail.com>
2026-03-08 16:19:18 +0900

    refactor: 미사용 domain 포트 인터페이스 선작성 제거

--- src/main/java/ojik/ojikback/domain/port/out/StadiumRepository.java
+++
@@ -1,8 +0,0 @@
-package ojik.ojikback.domain.port.out;
-
-import java.util.Optional;
-import ojik.ojikback.domain.entity.Stadium;
-
-public interface StadiumRepository {
-    Optional<Stadium> findById(Long stadiumId);
-}
--- src/main/java/ojik/ojikback/domain/port/out/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package ojik.ojikback.domain.port.out;
-
-import java.util.Optional;
-import ojik.ojikback.domain.entity.Team;
-
-public interface TeamRepository {
-    Optional<Team> findById(Long teamId);
-}
